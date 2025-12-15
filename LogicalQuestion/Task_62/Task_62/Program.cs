4ad84f8f485f29193eebb3a88f96c17bd1deacca
AKhute <anjali.khute@interbizconsulting.com>
2025-12-15 10:30:17 +0530

    feat: added logical question tasks

---
+++ LogicalQuestion/Task_62/Task_62/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Enter a string:");
+        string s = Console.ReadLine().ToLower();
+
+        char[] arr = s.ToCharArray();
+
+        foreach (char c in arr)
+        {
+            if (s.IndexOf(c) == s.LastIndexOf(c))
+            {
+                Console.WriteLine(c);
+                return;
+            }
+        }
+
+        Console.WriteLine('0');
+
+    }
+}
+
