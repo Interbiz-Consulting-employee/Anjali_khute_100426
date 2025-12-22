ebd870b554e57728e710c8136246cbaa9e9d00d1
AKhute <anjali.khute@interbizconsulting.com>
2025-12-22 21:20:57 +0530

    Get 10 Percentage by using Extension Method

---
+++ ExtensionMethodTask/ExtensionMethodTask/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+public static class NumberExtensions
+{
+    public static double TenPercent(this int number)
+    {
+        return number * 0.10;
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        double result = num.TenPercent();
+
+        Console.WriteLine(result);
+
+
+    }
+}
+
