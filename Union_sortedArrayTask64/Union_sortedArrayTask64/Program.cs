199c9ebc1ff327ef3056fc5ea45d19dcdcb56042
AKhute <anjali.khute@interbizconsulting.com>
2025-12-29 13:41:53 +0530

    union with sorted array - Task64

---
+++ Union_sortedArrayTask64/Union_sortedArrayTask64/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr1 = { 1, 2, 3, 4, 5 };
+        int[] arr2 = { 2, 3, 4, 5 };
+
+
+        // -- with union method
+
+        //var unionResult = arr1.Union(arr2);
+        //foreach (var r in unionResult)
+        //    Console.Write(r + " ");
+
+
+
+        // -- with hashset collection
+
+        HashSet<int> hs = new HashSet<int>();
+
+        foreach (int ar in arr1)
+        {
+            hs.Add(ar);
+        }
+
+        foreach (int ar in arr2)
+        {
+            hs.Add(ar);
+        }
+
+        List<int> l = new List<int>(hs);
+        l.Sort();
+
+        foreach (int ar in l)
+            Console.Write(ar + " ");
+    }
+}
