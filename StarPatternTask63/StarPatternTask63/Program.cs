c51432a079a362823df611ce02e50018c892f337
AKhute <anjali.khute@interbizconsulting.com>
2025-12-29 11:40:37 +0530

    star pattern task 63

---
+++ StarPatternTask63/StarPatternTask63/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 5;
+
+        // Upper 
+        for (int i = 1; i <= n; i++)
+        {
+        
+            for (int j = 1; j <= i - 1; j++)
+            {
+                Console.Write(" ");
+            }
+
+         
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+
+
+        // Lower 
+        for (int i = n - 1; i >= 1; i--)
+        {
+          
+            for (int j = 1; j <= i - 1; j++)
+            {
+                Console.Write(" ");
+            }
+
+          
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+
