6899701e9b1b3982ce13d0b90383379d4fa39f22
AKhute <anjali.khute@interbizconsulting.com>
2026-03-03 11:56:05 +0530

    find divisor task-74 that given by deepak sir

---
+++ LogicalQuestion/MissingDivisiorTask74/MissingDivisiorTask74/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MissingDivisor
+{
+    static void Main()
+    {
+       
+        Console.Write("Enter n: ");
+        long n;
+        while (!long.TryParse(Console.ReadLine(), out n) || n <= 0)
+        {
+            Console.WriteLine("Please enter a valid positive number:");
+        }
+
+        Console.WriteLine("Enter divisors separated by space:");
+        string[] input = Console.ReadLine().Split(' ');
+
+        HashSet<long> arrSet = new HashSet<long>();
+
+        foreach (string s in input)
+        {
+            if (long.TryParse(s, out long value))
+                arrSet.Add(value);
+        }
+
+        long missingDivisor = -1;
+
+        for (long i = 1; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                long pair = n / i;
+
+                if (!arrSet.Contains(i))
+                {
+                    missingDivisor = i;
+                    break;
+                }
+
+                if (!arrSet.Contains(pair))
+                {
+                    missingDivisor = pair;
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine("Missing Divisor: " + missingDivisor);
+    }
+}
