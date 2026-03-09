acbe0f9dffa382972298f71d68be01897c5c7d91
AKhute <anjali.khute@interbizconsulting.com>
2026-03-09 12:31:19 +0530

    power of two task-76 that given by sanskar sir

---
+++ LogicalQuestion/PowerOfTwoTask76/PowerOfTwoTask76/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter an integer: ");
+
+        if (!long.TryParse(Console.ReadLine(), out long n))
+        {
+            Console.WriteLine("Invalid input. Please enter a number.");
+            return;
+        }
+
+  
+        if (n < -2147483648 || n > 2147483647)
+        {
+            Console.WriteLine("Input is outside the allowed constraint range.");
+            return;
+        }
+
+        bool result = IsPowerOfTwo((int)n);
+
+        Console.WriteLine(result);
+    }
+
+    static bool IsPowerOfTwo(int n)
+    {
+        if (n <= 0)
+            return false;
+
+        return (n & (n - 1)) == 0;
+    }
+}
