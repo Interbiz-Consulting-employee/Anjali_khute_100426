b9bfa97b910f77c92b9104045895ee74a20ca91a
AKhute <anjali.khute@interbizconsulting.com>
2026-03-10 09:32:51 +0530

    Nim game task-77 that given by sanskar sir

---
+++ LogicalQuestion/NimGameTask77/NimGameTask77/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number of stones: ");
+
+        if (long.TryParse(Console.ReadLine(), out long n))
+        {
+            if (n < 1 || n > 2147483647)
+            {
+                Console.WriteLine("Input must be between 1 and 2^31 - 1");
+                return;
+            }
+
+            bool result = CanWinNim(n);
+
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+        }
+    }
+
+    static bool CanWinNim(long n)
+    {
+        return n % 4 != 0;
+    }
+}
